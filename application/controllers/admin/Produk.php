de9017144e98173cddba940bc1e6169e89c980a8
Ahmad Indra Nurfauzi <ahmadindran@student.telkomuniversity.ac.id>
2022-01-19 14:00:59 +0700

    Upload add produk

--- application/controllers/admin/Produk.php
+++ application/controllers/admin/Produk.php
@@ -22,9 +22,11 @@ class Produk extends CI_Controller
 
     function tambah()
     {
+        // $img = $_FILES['file'];
         $img = 'assets/uploads/' . $this->input->post('id');
         $config['allowed_types'] = 'jpg|png';
-        $config['upload_path'] = 'assets/upload/';
+        $config['upload_path'] = 'assets/upload/produk/';
+        $config['remove_space'] = TRUE;
         $config['max_size'] = 50000;
         $this->upload->initialize($config);
         $this->load->library('upload', $config);
@@ -35,15 +37,29 @@ class Produk extends CI_Controller
         $this->form_validation->set_rules('id', 'ID', 'required');
         $this->form_validation->set_rules('nama', 'Nama Produk', 'required');
         $this->form_validation->set_rules('stock', 'Stock', 'required');
+        $this->form_validation->set_rules('file', 'Foto', 'required');
+
         if ($this->form_validation->run() == FALSE && !$this->upload->do_upload('file')) {
-            print_r($this->upload->display_errors());
+            // print_r($this->upload->display_errors());
             $this->load->view('templates/header', $data);
             $this->load->view('admin/navbar');
             $this->load->view('admin/produk/add');
             $this->load->view('templates/footer');
         } else {
             $this->upload->do_upload('file');
-            $this->Produk_model->addDataProduk();
+            $gambar = $this->upload->data();
+            $gambar = $gambar['file_name'];
+            $input = array(
+                "id" => $this->input->post('id', true),
+                "nama" => $this->input->post('nama', true),
+                "brand" => $this->input->post('brand', true),
+                "kategori" => $this->input->post('kategori', true),
+                "stock" => $this->input->post('stock', true),
+                "aktif" => $this->input->post('active', true),
+                "status" => '1',
+                "foto" => $gambar
+            );
+            $this->Produk_model->addDataProduk($input);
             $this->session->set_flashdata('produk', 'Ditambahkan');
             redirect('admin/produk/index');
         }
--- application/models/Produk_model.php
+++ application/models/Produk_model.php
@@ -5,6 +5,7 @@ class Produk_model extends CI_Model
     function getAllProduk()
     {
         return $query = $this->db->get('produk')->result_array();
+        // return $query = $this->db->get_where('produk', array('status' => $status));
     }
 
     function getBrandForProduk()
@@ -22,19 +23,21 @@ class Produk_model extends CI_Model
         return $this->db->get_where('produk', ['id' => $id])->row_array();
     }
 
-    function addDataProduk()
+    function addDataProduk($input)
     {
-        $data = [
-            "id" => $this->input->post('id', true),
-            "nama" => $this->input->post('nama', true),
-            "brand" => $this->input->post('brand', true),
-            "kategori" => $this->input->post('kategori', true),
-            "stock" => $this->input->post('stock', true),
-            "aktif" => $this->input->post('active', true),
-            "status" => '1'
-            // "foto" => $this->input->post('foto', true)
-        ];
-        $this->db->insert('produk', $data);
+        // $file
+        // $data = [
+        //     "id" => $this->input->post('id', true),
+        //     "nama" => $this->input->post('nama', true),
+        //     "brand" => $this->input->post('brand', true),
+        //     "kategori" => $this->input->post('kategori', true),
+        //     "stock" => $this->input->post('stock', true),
+        //     "aktif" => $this->input->post('active', true),
+        //     "status" => '1',
+        //     // "foto" => $this->input->post('file')
+        //     "foto" => $file['file_name']
+        // ];
+        return $this->db->insert('produk', $input);
     }
 
     function editDataProduk()
@@ -59,6 +62,6 @@ class Produk_model extends CI_Model
             "status" => "2"
         ];
         $this->db->where('id', $id);
-        $this->db->update('produk', $data);        
+        $this->db->update('produk', $data);
     }
 }
